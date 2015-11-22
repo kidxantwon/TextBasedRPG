01f096a65c08069728c136f25566aa7aec0e98fa
kidxantwon <antonio.shaw@outlook.com>
2015-11-21 21:10:51 -0500

    getting the game to run

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool start = true;
+            while (start == true) ;
+            {
+                Console.WriteLine("Do you want to start the game? y or n?");
+                string startOptions = Console.ReadLine();
+                if (startOptions == "y")
+                {
+                    Console.WriteLine("Officer: So tell me how did it all being?");
+                    Console.WriteLine("Player: Well I ran out of gum so i went to the store.");
+
+
+                }
+                else
+                {
+                    start = false;
+                }
+            }
+        }
+    }
+}
